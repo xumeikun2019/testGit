d6bb5cd6ed84c95d3bece5da99681a3005b789e6
mk <pftmkx@yeah.net>
2019-06-05 22:01:46 +0800

    first c

---
+++ src/main/java/com/example/demo/doem/dddd.java
@@ -0,0 +1,9 @@
+package com.example.demo.doem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class dddd {
+    List<Map<String,Object>>  list = new ArrayList<Map<String, Object>>();
+}
---
+++ src/main/java/com/example/demo/doem/util/JsonToTree.java
@@ -0,0 +1,7 @@
+package com.example.demo.doem.util;
+
+public class JsonToTree {
+
+
+
+}
